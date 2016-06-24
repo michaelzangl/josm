0955c5682a19d794fa90a34db6a9e30ab6424b6d
Michael Zangl <michael.zangl@student.kit.edu>
2016-06-24 07:03:31 +0200

    Debug info to toggle dialog.

--- src/org/openstreetmap/josm/gui/dialogs/ToggleDialog.java
+++ src/org/openstreetmap/josm/gui/dialogs/ToggleDialog.java
@@ -308,6 +308,12 @@ public class ToggleDialog extends JPanel implements ShowHideButtonListener, Help
                 showNotify();
             }
         }
+
+        @Override
+        public String toString() {
+            return "ToggleDialogAction [" + ToggleDialog.this.toString() + "]";
+        }
+
     }
 
     /**
