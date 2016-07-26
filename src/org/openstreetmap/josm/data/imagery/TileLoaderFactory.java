d12ea0a5b7041e3199b08b74675eaf171644580f
Michael Zangl <michael.zangl@student.kit.edu>
2016-07-26 13:16:17 +0200

    Start extracting coordinate conversion out of tile source.

--- src/org/openstreetmap/josm/data/imagery/TileLoaderFactory.java
+++ src/org/openstreetmap/josm/data/imagery/TileLoaderFactory.java
@@ -12,17 +12,22 @@ import org.openstreetmap.gui.jmapviewer.interfaces.TileLoaderListener;
  * @author Wiktor Niesiobędzki
  * @since 8526
  */
+@FunctionalInterface
 public interface TileLoaderFactory {
 
     /**
      * @param listener that will be notified, when tile has finished loading
      * @return TileLoader that notifies specified listener
+     * @deprecated Use {@link #makeTileLoader(TileLoaderListener, Map)}
      */
-    TileLoader makeTileLoader(TileLoaderListener listener);
+    @Deprecated
+    default TileLoader makeTileLoader(TileLoaderListener listener) {
+        return makeTileLoader(listener, null);
+    }
 
     /**
      * @param listener that will be notified, when tile has finished loading
-     * @param headers that will be sent with requests to TileSource
+     * @param headers that will be sent with requests to TileSource. <code>null</code> indicates none
      * @return TileLoader that uses both of above
      */
     TileLoader makeTileLoader(TileLoaderListener listener, Map<String, String> headers);
